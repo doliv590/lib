f529cc15d1a6547cf744f8a1ad482fb2d0308e01
olivierde <olivier.delabasserue@yahoo.fr>
2019-01-10 23:10:26 +0100

    exo password

---
+++ 02_password.rb
@@ -0,0 +1,61 @@
+
+puts "c'est quoi ton nom ?"
+login = gets.chomp
+def ask_login(login)
+ask_login(login)
+end
+
+puts "encore !"
+signup = gets.chomp
+def try_signup(signup)
+try_signup(signup)
+end
+
+def welldone(bravo)
+	bravo = "bah voilà, TMTC!"
+end
+
+def machin 	
+ while ask_login != try_signup 
+	  ask_login(login) end
+		if login == signup
+		 welldone(bravo) end 
+
+
+
+
+#
+#def ask_signup(signup)
+#	puts"Mon ami, si tu veux découvrir un secret, donne moi ton nom"
+#	signup = gets.chomp
+#end
+
+
+def try_login(login)
+	puts"Mon ami, écris à nouveau ton nom, si tu t'en rappelles alors je te dis un vrai secret, sinon... tu essaies à nouveau."
+	login = gets.chomp
+end
+
+
+
+#machin2(login, signup)
+#end
+
+#def machin2(truk, bidul)
+#puts truk
+#puts bidul
+#while login != signup 
+#	ask_login
+#		if login == signup
+#		 welcomescreen
+#end
+
+
+
+
+#def welcomescreen(bravo)
+#	puts "Mais tu croyais vraaaaiment que j'allais te dire un truc cool ?"
+#end
+#a = 1
+#b = 5
+#machin(a, b)
